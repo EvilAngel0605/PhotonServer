59638ee085a3fc5ed6d3404910f44f6fc5618e32
EvilAngel0605 <1125018106@qq.com>
2018-07-15 23:41:18 +0800

    自己创建PhotonServer应用

---
+++ Photon游戏服务器/MyGameServer/ClientPeer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+
+namespace MyGameServer
+{
+    class ClientPeer : Photon.SocketServer.ClientPeer
+    {
+        /// <summary>
+        /// 构造函数,初始化对象
+        /// </summary>
+        /// <param name="initRequest"></param>
+        public ClientPeer(InitRequest initRequest) : base(initRequest)
+        {
+
+        }
+        /// <summary>
+        /// 处理客户端的断开连接
+        /// </summary>
+        /// <param name="reasonCode"></param>
+        /// <param name="reasonDetail"></param>
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// 处理客户端的请求(连接服务器之后)
+        /// </summary>
+        /// <param name="operationRequest"></param>
+        /// <param name="sendParameters"></param>
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Photon游戏服务器/MyGameServer/MyGameServer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+using ExitGames.Logging;
+using System.IO;
+using ExitGames.Logging.Log4Net;
+using log4net.Config;
+
+namespace MyGameServer
+{
+    /// <summary>
+    ///所有的Server端主类都要继承自ApplicationBase
+    /// </summary>
+    class MyGameServer : ApplicationBase
+    {
+        private static readonly ILogger log = LogManager.GetCurrentClassLogger();
+        /// <summary>
+        /// 当一个客户端请求连接
+        /// 使用PeerBase表示和一个客户端连接
+        /// </summary>
+        /// <param name="initRequest"></param>
+        /// <returns></returns>
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            return new ClientPeer(initRequest);
+            log.Info("新的客户端连接！");
+        }
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        protected override void Setup()
+        {
+            //日志的初始化
+            log4net.GlobalContext.Properties["Photon:ApplicationLogPath"] = Path.Combine(this.ApplicationRootPath, "bin_Win64","log");
+            FileInfo fileInfo = new FileInfo(Path.Combine(this.BinaryPath, "log4net.config"));
+            if(fileInfo.Exists)
+            {
+                LogManager.SetLoggerFactory(Log4NetLoggerFactory.Instance);//让photo你知道使用的哪个插件
+                XmlConfigurator.ConfigureAndWatch(fileInfo);//让log4net插件读取配置文件
+            }
+            log.Info("Set Up!");
+        }
+        /// <summary>
+        /// Server端关闭的时候
+        /// </summary>
+        protected override void TearDown()
+        {
+            log.Info("服务器关闭应用！");
+        }
+    }
+}
